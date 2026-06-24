36fa9e5602a1f4cf583eea560af28372144e13e8
Francisco Arechiga <francisco.arechiga@gmail.com>
2026-06-24 16:11:11 -0500

    Add appConfig with Supabase and Turnstile keys

---
+++ src/config.js
@@ -0,0 +1,5 @@
+export const appConfig = {
+  supabaseUrl: "https://qenslomxfajfoujlwsgm.supabase.co",
+  supabasePublishableKey: "sb_publishable_5YDfTctG_NDLfO1OYNLgOQ_CoypNp8g",
+  turnstileSiteKey: "0x4AAAAAADqg98tdyQUXxyV1"
+};
